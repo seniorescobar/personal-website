051dc24f14309b526dc0ac39e4dcf83611669358
Aleksandar Jelić <aleksandar@jelic.email>
2020-11-11 00:20:34 +0100

    responsivness

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,52 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div id="sidebar" class="col-xl-3 offset-xl-1">
-        <section id="home" class="home d-flex align-items-center text-center">
-          <div class="row">
-            <div class="col-md-3 col-lg-12">
-              <img
-                src="./assets/images/memoji.png"
-                alt="Profile picture"
-                class="img-fluid rounded-circle mb-4"
-              />
-            </div>
-            <div class="col-md-9 col-lg-12">
-              <h1 class="text-dark mb-2 mt-0">Aleksandar Jelić</h1>
-              <span>Web Developer</span>
-
-              <ul class="social-icons list-inline mb-0 mt-2">
-                <li class="list-inline-item">
-                  <a
-                    target="_blank"
-                    href="https://www.linkedin.com/in/aleksandar-jeli%C4%87-b0445b76/"
-                    class="text-dark"
-                  >
-                    <font-awesome-icon :icon="['fab', 'linkedin']" />
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a
-                    target="_blank"
-                    href="https://github.com/seniorescobar"
-                    class="text-dark"
-                  >
-                    <font-awesome-icon :icon="['fab', 'github']" />
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a
-                    target="_blank"
-                    href="mailto:aleksandar@jelic.email"
-                    class="text-dark"
-                  >
-                    <font-awesome-icon :icon="['fas', 'envelope']" />
-                  </a>
-                </li>
-              </ul>
-            </div>
+      <div id="sidebar" class="col-12 col-xl-3 offset-xl-1 d-flex align-items-center justify-content-center mt-5 mt-lg-0">
+        <div class="row text-center">
+          <div class="col-md-3 col-lg-12">
+            <img
+              src="./assets/images/memoji.png"
+              alt="Profile picture"
+              class="img-fluid rounded-circle mb-4"
+            />
           </div>
-        </section>
+          <div class="col-md-9 col-lg-12 text-center text-md-left text-lg-center">
+            <h1 class="text-dark mb-2 mt-0">Aleksandar Jelić</h1>
+            <span>Web Developer</span>
+
+            <ul class="social-icons list-inline mb-0 mt-2">
+              <li class="list-inline-item">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/aleksandar-jeli%C4%87-b0445b76/"
+                  class="text-dark"
+                >
+                  <font-awesome-icon :icon="['fab', 'linkedin']" />
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a
+                  target="_blank"
+                  href="https://github.com/seniorescobar"
+                  class="text-dark"
+                >
+                  <font-awesome-icon :icon="['fab', 'github']" />
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a
+                  target="_blank"
+                  href="mailto:aleksandar@jelic.email"
+                  class="text-dark"
+                >
+                  <font-awesome-icon :icon="['fas', 'envelope']" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div class="col-xl-6 offset-xl-5 pt-5">
+      <div class="col-xl-6 offset-xl-5 pt-xl-5">
         <AboutMe />
         <Experience />
       </div>
@@ -76,11 +74,6 @@ export default {
 @import "./assets/scss/variables";
 @import "node_modules/bootstrap/scss/bootstrap";
 
-section.home {
-  height: 100vh;
-  min-height: 100vh;
-}
-
 .social-icons {
   li {
     a {
@@ -91,7 +84,8 @@ section.home {
 
 @include media-breakpoint-up(xl) {
   #sidebar {
-    position: fixed;
+	position: fixed;
+	min-height: 100vh;
   }
 }
 
