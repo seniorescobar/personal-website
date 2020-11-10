48977e86055a53a0a06cb7d3439e969219acf96f
Aleksandar Jelić <aleksandar@jelic.email>
2020-11-10 13:07:31 +0100

    imported bolby theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import './assets/css/style.scss'
+
+const app = createApp(App)
+
+app.mount('#app')
