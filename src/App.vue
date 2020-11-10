f4f5580e3909519a65d5a223f1513cccbcef4b7b
Aleksandar Jelić <aleksandar@jelic.email>
2020-11-10 17:40:29 +0100

    removed _base.scss

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="row">
-    <div class="col-lg-3 offset-lg-1">
-      <!-- section home -->
-      <section id="home" class="home d-flex light align-items-center">
-        <div class="container">
-          <!-- intro -->
-          <div class="intro">
-            <!-- avatar image -->
-            <img src="./assets/images/avatar-2.svg" alt="Bolby" class="mb-4" />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-3 offset-lg-1">
+        <!-- section home -->
+        <section id="home" class="home d-flex light align-items-center">
+          <div class="container">
+            <!-- intro -->
+            <div class="intro">
+              <!-- avatar image -->
+              <img
+                src="./assets/images/avatar-2.svg"
+                alt="Bolby"
+                class="mb-4"
+              />
 
-            <!-- info -->
-            <h1 class="mb-2 mt-0">Aleksandar Jelić</h1>
-            <span>Web Developer</span>
+              <!-- info -->
+              <h1 class="mb-2 mt-0">Aleksandar Jelić</h1>
+              <span>Web Developer</span>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
-    <div class="col-lg-7">
-      <!-- section about -->
-      <AboutMe />
+        </section>
+      </div>
+      <div class="col-lg-7">
+        <!-- section about -->
+        <AboutMe />
 
-      <!-- section experience -->
-      <Experience />
+        <!-- section experience -->
+        <Experience />
+      </div>
     </div>
+
+    <footer class="footer light">
+      <div class="container">
+        <span class="copyright">© 2020 TODO</span>
+      </div>
+    </footer>
   </div>
-
-  <footer class="footer light">
-    <div class="container">
-      <span class="copyright">© 2020 TODO</span>
-    </div>
-  </footer>
 </template>
 
 <script>
